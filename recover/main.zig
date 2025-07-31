1e67206f284cdce999d5b099ee534f08b40d1a9b
Nathaniel Ketema <nathanielketema@gmail.com>
2025-07-31 18:03:46 -0500

    feat: first commit of recover

---
+++ recover/main.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+//Open memory card 
+//Repeat until end of card 
+//    Read 512 bytes into a buffer 
+//    If start of new JPEG 
+//        If first JPEG 
+//            .... 
+//        Else 
+//            ....
+//    Else 
+//        If already found JPEG
+//            ....
+//
+
+pub fn main () void {
+}
