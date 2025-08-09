823daeba4a7a9b32ceaf34eda95ce3fd97228632
Nathaniel Ketema <nathanielketema@gmail.com>
2025-08-09 17:27:07 -0500

    feat: modify build script to be able to run all text files

--- speller/build.zig
+++ speller/build.zig
@@ -12,10 +12,28 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(exe);
 
+    const build_steps = .{
+        .run = b.step("run", "Run speller on a single file"),
+        .batch = b.step("batch", "Run speller on all data/texts file"),
+    };
+
     const run_exe = b.addRunArtifact(exe);
     if (b.args) |args| {
         run_exe.addArgs(args);
     }
-    const run_step = b.step("run", "Run speller");
-    run_step.dependOn(&run_exe.step);
+    build_steps.run.dependOn(&run_exe.step);
+
+    const script_file = b.addWriteFiles().add("batch.sh", 
+        \\#!/bin/bash
+        \\for file in data/texts/*.txt; do
+        \\    echo "Processing: $file"
+        \\    ./zig-out/bin/speller "$file" > "outputs/$(basename "$file")"
+        \\done
+    );
+
+    const run_batch = b.addSystemCommand(&.{"bash"});
+    run_batch.addFileArg(script_file);
+
+    run_batch.step.dependOn(&exe.step);
+    build_steps.batch.dependOn(&run_batch.step);
 }
