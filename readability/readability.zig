cd05a0b0cb042d3324b7cc9f2233de8b70394a77
Nathaniel Ketema <nathanielketema@gmail.com>
2025-08-12 19:22:07 -0500

    feat: all tests pass :)

--- readability/readability.zig
+++ readability/readability.zig
@@ -2,12 +2,11 @@ const std = @import("std");
 const print = std.debug.print;
 const testing = std.testing;
 const assert = std.debug.assert;
-const math = std.math;
 const stdin = std.io.getStdIn().reader();
 
 const max_words = 500;
 
-const Grade = union(enum) {
+const Grade = enum {
     before_grade_1,
     grade_1,
     grade_2,
@@ -85,23 +84,31 @@ fn readability_grade(input: []const u8) Grade {
     var words: u16 = 0;
     var sentences: u16 = 0;
 
-    // todo: remove
-    letters += 1;
-    words += 1;
-    sentences += 1;
-
     for (input) |c| {
-        if (std.ascii.isAlphabetic(c)) {}
+        if (std.ascii.isAlphabetic(c)) {
+            letters += 1;
+        } 
+        if (std.ascii.isWhitespace(c)) {
+            words += 1;
+        } 
+        if (c == '?' or c == '!' or c == '.') {
+            sentences += 1;
+        }
     }
 
-    // Coleman-Liau index:
-    //    index = 0.0588 * L - 0.296 * S - 15.8
+    // To adjust for the last word, since there is no space after it
+    words += 1;
+
+    // Coleman-Liau index: index = 0.0588 * L - 0.296 * S - 15.8
     // where L is the average number of letter in 100 words, (letters / words) * 100
     //       S is the average number of sentences in 100 words, (sentences / words) * 100
     assert(words != 0);
-    const L = @divTrunc(@as(f16, @floatFromInt(letters)), @as(f16, @floatFromInt(words))) * 100;
-    const S = @divTrunc(@as(f16, @floatFromInt(sentences)), @as(f16, @floatFromInt(words))) * 100;
-    const index: i32 = @intFromFloat(0.0588 * L - 0.296 * S - 15.8);
+    const L = (@as(f16, @floatFromInt(letters)) / @as(f16, @floatFromInt(words))) * 100;
+    const S = (@as(f16, @floatFromInt(sentences)) / @as(f16, @floatFromInt(words))) * 100;
+
+    assert(L != 0);
+    assert(S != 0);
+    const index: i32 = @intFromFloat(@round(0.0588 * L - 0.296 * S - 15.8));
 
     if (index < 1) {
         return .before_grade_1;
@@ -140,7 +147,7 @@ test "grade 2" {
     try testing.expect(
         readability_grade(
             "Would you like them here or there? I would not like them here or there. " ++
-                "I would not like them anywhere. ",
+                "I would not like them anywhere.",
         ) == .grade_2,
     );
 }
@@ -191,7 +198,7 @@ test "grade 8" {
                 "football were assuaged, he was seldom self-conscious about his injury. " ++
                 "His left arm was somewhat shorter than his right; when he stood or walked, " ++
                 "the back of his hand was at right angles to his body, his thumb " ++
-                "parallel to his thigh. ",
+                "parallel to his thigh.",
         ) == .grade_8,
     );
 }
@@ -200,7 +207,7 @@ test "grade 9" {
     try testing.expect(
         readability_grade(
             "There are more things in Heaven and Earth, Horatio, " ++
-                "than are dreamt of in your philosophy. ",
+                "than are dreamt of in your philosophy.",
         ) == .grade_9,
     );
 }
@@ -212,7 +219,7 @@ test "grade 10" {
                 "Winston Smith, his chin nuzzled into his breast in an effort to escape " ++
                 "the vile wind, slipped quickly through the glass doors of Victory Mansions, " ++
                 "though not quickly enough to prevent a swirl of gritty dust from entering " ++
-                "along with him. ",
+                "along with him.",
         ) == .grade_10,
     );
 }
@@ -223,7 +230,7 @@ test "grade 16 plus" {
             "A large class of computational problems involve the determination of " ++
                 "properties of graphs, digraphs, integers, arrays of integers, " ++
                 "finite families of finite sets, boolean formulas and elements of " ++
-                "other countable domains. ",
+                "other countable domains.",
         ) == .grade_16_plus,
     );
 }
